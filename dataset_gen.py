4c657aa21126ff9c454ed74ec819c2bdd0c350ca
Aaron Green <aaron.green281@gmail.com>
2018-02-02 02:02:26 -0600

    fix data set

--- dataset_gen.py
+++ dataset_gen.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 import numpy as np
 import pandas as pd
 import os
+import scipy
 
 DIR_PATH = './dm_stories_tokenized/'
 english_stopwords = set(stopwords.words('english'))
@@ -20,7 +21,7 @@ if __name__ == "__main__":
 
 		for line in story:
 			filtered_line = []
-			for word in line.split(" "):
+			for word in line.strip().split(" "):
 				if word not in english_stopwords:
 					filtered_line.append(word)
 
@@ -32,38 +33,34 @@ if __name__ == "__main__":
 
 	documents = [''.join(story) for story in stories]
 
-	doc_vectorizer = CountVectorizer()
-	vec_stories = doc_vectorizer.fit_transform(documents)
+	vectorizer = CountVectorizer()
+	vec_stories = vectorizer.fit_transform(documents)
 
 	lda = LDA(n_topics = len(input_files))
 	lda.fit(vec_stories)
+	topic_dist = lda.components_ / lda.components_.sum(axis=1)[:,np.newaxis]
 
-	sen_vectorizer = CountVectorizer()
-	vec_sentences = sen_vectorizer.fit_transform(sentences)
-
-	word_to_index = sen_vectorizer.vocabulary_
-	index_to_word = np.chararray(len(word_to_index), itemsize=100)
-	for word in word_to_index:
-	    index = word_to_index[word]
-	    index_to_word[index] = word
+	vec_sentences = vectorizer.transform(sentences)
 
 	sen_topics = lda.transform(vec_sentences)
+	most_likely_topics = np.argmax(sen_topics, axis=1)
 
-	w_z = lda.components_ / lda.components_.sum(axis=1)[:, np.newaxis]
+	tokenized_sentences = [sentence.split(" ") for sentence in sentences]
+	df = pd.DataFrame(columns=['sentences', 'dists'])
+	df['sentences'] = sentences
 
-	top_word_args = np.argsort(w_z, axis=1)[:,-1*N_TOP_WORDS:]
-	top_words = np.chararray((w_z.shape[0], N_TOP_WORDS, 2), itemsize=100)
-	for i in range(len(input_files)):
-	    top_words[i,:,0] = index_to_word[top_word_args[i]]
-	    top_words[i,:,1] = w_z[i,top_word_args[i]]
+	word_dists = []
+	for i, token_sentence in enumerate(tokenized_sentences):
+		if (i % 100 == 0):
+			print("Finished: " + str(i))	
+		if (''.join(token_sentence).strip() == ''):
+			continue
+		word_vec = vectorizer.transform(token_sentence).todense()
+		topic_index = most_likely_topics[i]
+		word_dist = topic_dist[topic_index,:]
+		word_probs = word_vec.dot(word_dist[:,np.newaxis])
+		df.loc[i, 'dists'] = np.log(word_probs).reshape(word_probs.shape[0]).tolist()[0]
 
-	df = pd.DataFrame()
-	df['sentences'] = pd.Series(sentences)
-	for i in range(len(sen_topics.T)):
-		df['topic-' + str(i)] = pd.Series(sen_topics.T[i])
-	for i in range(len(top_words)):
-		df['topic-' + str(i) + '-words'] = pd.Series(top_words[i][:,0])
-		df['topic-' + str(i) + '-wordprobs'] = pd.Series(top_words[i][:,1])
-
+	df.dropna(axis=0, how='any', inplace=True)
 	df.to_csv('data.csv', index=False)
 
